60951d3ac672154f9e6ddbfbbeb9de576db7a48f
yaroslav <yaseliwanoff@gmail.com>
2024-05-11 17:01:36 +0300

    added image folder and add image in models application

--- myapp/models.py
+++ myapp/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.CharField(max_length=200)
+    image = models.ImageField(blank=True, upload_to='images')
 
     def __str__(self):
         return self.name
